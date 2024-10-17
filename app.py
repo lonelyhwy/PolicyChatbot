1d1a3c976caf8729b3d57f36eb282bd6800dd320
huwangyang <huwangyanghwy@outlook.com>
2024-10-17 06:07:06 +0000

    use streamer rather than pipelinie

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ with demo:
 
     # Event handler for submitting text and generating response
     submit_button.click(pdf_chatbot.add_text, inputs=[chat_history, txt], outputs=[chat_history], queue=False).\
-        success(pdf_chatbot.generate_response, inputs=[chat_history, txt, uploaded_pdf], outputs=[chat_history, txt]).\
+        success(pdf_chatbot.generate_response, inputs=[chat_history, txt, uploaded_pdf], outputs=[chat_history, txt], queue=True).\
         success(pdf_chatbot.render_file, inputs=[uploaded_pdf], outputs=[show_img])
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.queue().launch()
